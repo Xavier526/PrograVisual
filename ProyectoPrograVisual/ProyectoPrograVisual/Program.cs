1e2f102e9f26357eecbfa94a0c83cd409dbf0ebb
Xavier526 <63320216+Xavier526@users.noreply.github.com>
2020-06-25 13:52:56 -0500

    Add files via upload

---
+++ ProyectoPrograVisual/ProyectoPrograVisual/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreandoSistema
+{
+    class Principal
+
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese Sus Nombres: ");
+
+            string nombres = Convert.ToString(Console.ReadLine());
+
+            //*************//
+
+            Console.Write("Ingrese sus Apellidos: ");
+
+            string apellidos = Convert.ToString(Console.ReadLine());
+
+            //*************//
+
+            Console.Write("Ingrese su Edad: ");
+            double numEdad = Convert.ToDouble(Console.ReadLine());
+
+            double resultado;
+
+            resultado = numEdad;
+
+            //*************//
+
+            Console.WriteLine("Sus Nombres Completos son: " + nombres + apellidos);
+
+            Console.WriteLine("su edad es " + resultado);
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
+
+
